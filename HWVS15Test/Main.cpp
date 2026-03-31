b74e7d4598e7156e712e54d09ecf85d5b4763856
YCaTGameRT <ycatgamert@gmail.com>
2026-03-31 05:09:42 +0500

    tests

--- HWVS15Test/Main.cpp
+++ HWVS15Test/Main.cpp
@@ -3,14 +3,33 @@
 #include "Dog.h"
 #include "Cat.h"
 #include "Bird.h"
+#include "FlyingCat.h"
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	Bird one("CaT", 120, 44, 60, "Yellow", "Dodo", false, true);
-	one.Move(13);
-	one.Fly(22);
+	Animal animal("qwerty", 12, 122, 71);
+	Dog dog("wasd", 41, 33, 12, "black", 44, true);
+	Cat CaT("CaT", 15, 160, 70, true, "pink", true);
+	Bird bird("xyz", 120, 44, 60, "Yellow", "Dodo", false, true);
+	FlyingCat flyingCat("fc", 654, 32, 6, false, "gray", false, "orange", "fox", false, false, 4, 9, "green");
+	
+	animal.Eat(3);
+	animal.GoOlder(5);
+	animal.Move(2);
+	dog.RotatingAround();
+	dog.RunToOwner();
+	dog.TailWagging();
+	CaT.CatLove();
+	CaT.NoseColorChoose();
+	CaT.Scamper();
+	bird.BirdSing();
+	bird.Fly(4);
+	bird.Molting();
+	flyingCat.CatLove();
+	flyingCat.MyTails();
+	flyingCat.Eat(13);
 
 	system("pause");
 	return 0;
